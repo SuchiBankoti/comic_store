fe7ab936e45b5b58451f177302aa404e6d935f84
suchi <suchibankoti95@gmail.com>
2022-09-25 19:42:09 +0530

    css buy page

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom"
 import sign from "./sign.png"
-import { FaHome } from 'react-icons/fa'
+import { FaHome, FaBook } from 'react-icons/fa'
 import SignUpForm from "./SignUpForm";
 
 
@@ -9,13 +9,14 @@ export default function Navbar() {
 
     return (
         <nav className="navbar">
-            <Link to="/Library" className="link"><h1>LIBRARY</h1></Link>
-            <div>
+            <div className="nav-left">
+                <Link to="/Library" className="link"><h1>LIBRARY</h1></Link><FaBook />
+            </div>
+            <div className="nav-right">
                 <div>
                     <SignUpForm />
                 </div>
                 <Link to="/" ><FaHome className="homeicon" /></Link>
-
                 <img alt="" src={sign} />
 
             </div>
--- src/SideBook.js
+++ src/SideBook.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useContext } from "react";
 import { LibraryContext } from "./Contexts/LibraryContext";
+import { FaReadme, FaStar, FaStarHalf, FaCartArrowDown, FaEuroSign } from "react-icons/fa";
 export default function SideBook() {
 
     const { status, buy } = useContext(LibraryContext)
@@ -8,18 +9,29 @@ export default function SideBook() {
 
     return (
         <div className="buy-page">
-            <div>
-                <div><img alt="" src={process.env.PUBLIC_URL + `Images/${Book.bookCover}`} /></div>
-                <div className="book-details">
-                    <h1>{Book.title}</h1>
-                    <div>{Book.author}</div>
-                    <div>{Book.Rating}</div>
-                    <div>{Book.year}</div>
-                    <div>{Book.lang}</div>
-                    <p>{Book.About}</p>
-                    <button onClick={() => status(Book.id)} style={{ background: Book.read ? "red" : "grey" }} className="read-btn"></button>
+            <div className="buy-page-left">
+                <div className="buy-cover"><img alt="" src={process.env.PUBLIC_URL + `Images/${Book.bookCover}`} /></div>
+                <div>{Book.About}</div>
+                <div style={{ color: Book.read ? "pink" : "cadetblue" }} className="read-status">
+                    <FaReadme onClick={() => status(Book.id)} style={{ color: Book.read ? "#fd5e79" : "#306264" }} className="read-btn" />
+                    {Book.read ? "Currently reading" : "Read Me"}
                 </div>
             </div>
+
+            <div className="buy-page-right">
+                <h1>{Book.title}</h1>
+                <div><h2>Price:200<FaEuroSign /></h2><p>Tax included. Excluding Shipping cost</p></div>
+                <button className="add-btn">Add To Cart <FaCartArrowDown /></button>
+                <button className="buy-btn">Buy Now </button>
+                <div>
+                    <h4>Product information</h4>
+                    <p>{Book.author}</p>
+                    <div className="rating"><FaStar /><FaStar /><FaStar /><FaStarHalf /></div>
+                    <div className="year">{Book.year}</div>
+                    <div className="lang">Original-language: {Book.lang}</div>
+                </div>
+            </div>
+
         </div>)
 
 }
